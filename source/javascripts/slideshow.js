5a602d9dec63e754a6dd96942f50ad7d92caf29b
Sebastian Wallin <sebastian.wallin@gmail.com>
2014-03-24 23:06:02 -0700

    Make crew clickable as well

--- source/javascripts/slideshow.js
+++ source/javascripts/slideshow.js
@@ -66,7 +66,7 @@
     // main grid
     this.grid = this.el.querySelector( '#people' );
     // main grid items
-    this.gridItems = [].slice.call( this.grid.querySelectorAll( 'a' ) );
+    this.gridItems = [].slice.call( this.el.querySelectorAll( '.pop' ) );
     // items total
     this.itemsCount = this.gridItems.length;
     // slideshow grid
